f917717034bca9f4a845fad7a787dda2fdf585c6
AttixSpace <siem.fannes@gmail.com>
2023-08-25 14:01:35 +0200

    Eerste versie van fps

---
+++ character.gd
@@ -0,0 +1,80 @@
+extends CharacterBody3D
+
+var speed
+const WALK_SPEED = 7.0
+const Sprint_SPEED = 9.0
+const JUMP_VELOCITY = 6.5
+const SENSITIVITY = 0.003
+const JUMPCOUNTER = 3
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 9.8
+var jumps = JUMPCOUNTER
+
+
+const BASE_FOV = 75.0
+const FOV_CHANGE = 1.5
+
+
+@onready var head = $head
+@onready var camera = $head/Camera3D
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	jumpreset()
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and jumps > 0:
+		velocity.y = JUMP_VELOCITY
+		jumps -= 1
+
+
+
+	if Input.is_action_just_pressed("Sprint"):
+		speed = Sprint_SPEED
+	else:
+		speed = WALK_SPEED
+	
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+
+
+
+	var velocity_clamped = clamp(velocity.length(), 0.5, Sprint_SPEED * 2)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+
+	move_and_slide()
+	
+func jumpreset():
+	if is_on_floor():
+		jumps = JUMPCOUNTER
+	
+	
